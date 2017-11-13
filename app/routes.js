ccf12c29e4def016064cbfb9cd2fa9b404651447
Sean Mooney <smooney914@gmail.com>
2017-11-13 18:00:00 -0500

    Initial Commit, configured routes, created ejs views for home and events

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+//create new express router
+const express = require('express'),
+  router = express.Router(),
+  mainController = require('./controllers/main.controller')
+  eventsController = require('./controllers/events.controller');
+
+// export router
+module.exports = router;
+
+// define routes
+// main routes
+router.get('/', mainController.showHome);
+
+//event routes
+router.get('/events', eventsController.showEvents);
---
+++ server.js
@@ -0,0 +1,21 @@
+//dependencies
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8080,
+  expressLayouts = require('express-ejs-layouts');
+
+//configure application
+//tell express where to look for static assets
+app.use(express.static(__dirname + '/public'));
+
+//set ejs as our templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+//set routes
+app.use(require('./app/routes'));
+
+//start server
+app.listen(port, ()=> {
+  console.log(`App listening on http://localhost:${port}`);
+});
